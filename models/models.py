3fef76b624f4e3c3bfdb14a22415c237c91f54b7
dekarh <ekFuwotJimt4>
2022-03-08 22:39:49 +0400

    Модуль hr_pf, версия 0.0.9 от 27.02.2022:   Доделал (исправил системную ошибку) добавление projectgroup_id по аналогии с department_id:     - рефакторинг по ключевым словам  'parent', 'child', 'manager', 'member', 'jobs'     - попробовал импорт ссылок (extenal id) при загрузке One2many и Many2one полей через .xml

--- models/models.py
+++ models/models.py
@@ -20,12 +20,14 @@ class HrEmployeePf(models.Model):
     general_contact_pf = fields.Integer(string='id в адр.строке')
     officetown_id = fields.Many2one('hr_pf.officetown', string='Офис и Город')
     # Добавляем аналог  department_id - projectgroup_id. Для этого проводим рефакторинг по ключам:
-    # 'department_id', 'hr.department', 'Department'
+    # 'department_id', 'hr.department', 'Department', 'parent', 'child', 'manager', 'member', 'jobs'
+    parent_pg_id = fields.Many2one('hr.employee', 'Manager ProjectGroup')
+    child_pg_ids = fields.One2many('hr.employee', 'parent_pg_id', string='Subordinates')
     projectgroup_id = fields.Many2one('hr.projectgroup', string='ProjectGroup')
 
     @api.onchange('projectgroup_id')
     def _onchange_projectgroup(self):
-        self.parent_id = self.projectgroup_id.manager_id
+        self.parent_pg_id = self.projectgroup_id.manager_pg_id
 
 
 class HrJobPf(models.Model):
@@ -43,24 +45,24 @@ class ProjectGroup(models.Model):
     complete_name = fields.Char('Complete Name', compute='_compute_complete_name', store=True)
     active = fields.Boolean('Active', default=True)
     company_id = fields.Many2one('res.company', string='Company', index=True, default=lambda self: self.env.user.company_id)
-    parent_id = fields.Many2one('hr.projectgroup', string='Parent ProjectGroup', index=True)
-    child_ids = fields.One2many('hr.projectgroup', 'parent_id', string='Child ProjectGroup')
-    manager_id = fields.Many2one('hr.projectgroup', string='Manager', track_visibility='onchange')
-    member_ids = fields.One2many('hr.employee', 'projectgroup_id', string='Members', readonly=True)
+    parent_pg_id = fields.Many2one('hr.projectgroup', string='Parent ProjectGroup', index=True)
+    child_pg_ids = fields.One2many('hr.projectgroup', 'parent_pg_id', string='Child ProjectGroup')
+    manager_pg_id = fields.Many2one('hr.employee', string='Manager ProjectGroup', track_visibility='onchange')
+    member_pg_ids = fields.One2many('hr.employee', 'projectgroup_id', string='ProjectGroup Members', readonly=True)
     jobs_ids = fields.One2many('hr.job', 'projectgroup_id', string='Jobs')
     note = fields.Text('Note')
     color = fields.Integer('Color Index')
 
-    @api.depends('name', 'parent_id.complete_name')
+    @api.depends('name', 'parent_pg_id.complete_name')
     def _compute_complete_name(self):
         for projectgroup in self:
-            if projectgroup.parent_id:
-                projectgroup.complete_name = '%s / %s' % (projectgroup.parent_id.complete_name, projectgroup.name)
+            if projectgroup.parent_pg_id:
+                projectgroup.complete_name = '%s / %s' % (projectgroup.parent_pg_id.complete_name, projectgroup.name)
             else:
                 projectgroup.complete_name = projectgroup.name
 
-    @api.constrains('parent_id')
-    def _check_parent_id(self):
+    @api.constrains('parent_pg_id')
+    def _check_parent_pg_id(self):
         if not self._check_recursion():
             raise ValidationError(_('Error! You cannot create recursive projectgroups.'))
 
@@ -70,9 +72,9 @@ class ProjectGroup(models.Model):
         # the tracking allows to track+subscribe fields linked to a res.user record
         # An update of the limited behavior should come, but not currently done.
         projectgroup = super(ProjectGroup, self.with_context(mail_create_nosubscribe=True)).create(vals)
-        manager = self.env['hr.employee'].browse(vals.get("manager_id"))
-        if manager.user_id:
-            projectgroup.message_subscribe_users(user_ids=manager.user_id.ids)
+        manager_pg = self.env['hr.employee'].browse(vals.get("manager_pg_id"))
+        if manager_pg.user_id:
+            projectgroup.message_subscribe_users(user_ids=manager_pg.user_id.ids)
         return projectgroup
 
     @api.multi
@@ -83,26 +85,26 @@ class ProjectGroup(models.Model):
         # TDE note: auto-subscription of manager done by hand, because currently
         # the tracking allows to track+subscribe fields linked to a res.user record
         # An update of the limited behavior should come, but not currently done.
-        if 'manager_id' in vals:
-            manager_id = vals.get("manager_id")
-            if manager_id:
-                manager = self.env['hr.employee'].browse(manager_id)
+        if 'manager_pg_id' in vals:
+            manager_pg_id = vals.get("manager_pg_id")
+            if manager_pg_id:
+                manager_pg = self.env['hr.employee'].browse(manager_pg_id)
                 # subscribe the manager user
-                if manager.user_id:
-                    self.message_subscribe_users(user_ids=manager.user_id.ids)
+                if manager_pg.user_id:
+                    self.message_subscribe_users(user_ids=manager_pg.user_id.ids)
             # set the employees's parent to the new manager
-            self._update_employee_manager(manager_id)
+            self._update_employee_manager_pg(manager_pg_id)
         return super(ProjectGroup, self).write(vals)
 
-    def _update_employee_manager(self, manager_id):
+    def _update_employee_manager_pg(self, manager_pg_id):
         employees = self.env['hr.employee']
         for projectgroup in self:
             employees = employees | self.env['hr.employee'].search([
-                ('id', '!=', manager_id),
+                ('id', '!=', manager_pg_id),
                 ('projectgroup_id', '=', projectgroup.id),
-                ('parent_id', '=', projectgroup.manager_id.id)
+                ('parent_pg_id', '=', projectgroup.manager_pg_id.id)
             ])
-        employees.write({'parent_id': manager_id})
+        employees.write({'parent_pg_id': manager_pg_id})
 
 
 
